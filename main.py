3a7a0c6a1b7ebc164623f13fa9f3d861c66f5c8d
Filip Troníček <filip.tronicek@seznam.cz>
2020-05-27 19:05:36 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,9 +4,6 @@ import os
 
 base = (json.loads(requests.get("https://api.exchangeratesapi.io/latest?base=USD").text))
 
-print(base)
-
-csvarrs = []
 def createString(csvArr):
     fnlStr = ""
     for i,el in enumerate(csvArr):
@@ -16,12 +13,19 @@ def createString(csvArr):
         else:
             fnlStr += "\n"
     return fnlStr
-    
+
 for b in base['rates']:
-    csvarrs.append([base['date'], b, base['rates'][b]])
+    csvarrs = ([base['date'], b, base['rates'][b]])
     if not os.path.isdir("data/"+b):
         os.mkdir("data/"+b)
     filename = "data/"+b+"/rate.csv"
+
+    if not os.path.isfile(filename) and not os.access(filename, os.R_OK):
+        pf = open(filename, "w")
+    if os.stat(filename).st_size == 0:
+        WriteData = open(filename, "a")
+        WriteData.write("date, base, rates")
+        WriteData.close()
     WriteData = open(filename, "a")
     WriteData.write(createString(csvarrs))
     WriteData.close()
